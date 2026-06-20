c3440e66d8aae880e3bd87f1db868fda9dff90d1
lankamar <lankamar@gmail.com>
2026-06-20 00:02:24 -0300

    Agregado deploy a GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/shock-room-simulator/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
